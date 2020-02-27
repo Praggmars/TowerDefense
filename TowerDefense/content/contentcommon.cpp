b0dbd05fce8a549036d458cb0ff7655cfe3629c2
Babics Mátyás <praggmarsnegro@gmail.com>
2020-02-27 19:59:14 +0100

    Material loading fix

--- TowerDefense/content/contentcommon.cpp
+++ TowerDefense/content/contentcommon.cpp
@@ -10,7 +10,7 @@ namespace TowerDefense
 			auto texture = gfx::Texture::CreateColoredTexture(graphics, { 100, 125, 225, 255 }, 16);
 			auto normalmap = gfx::Texture::CreateDefaultNormalmap(graphics, 16);
 			gfx::MaterialData materialData = { mth::float4(1.0f), mth::float4(), 1.0f, 1.0f };
-			gfx::Material::P material = gfx::Material::CreateP(texture, normalmap, materialData);
+			gfx::Material::P material;
 			gfx::ModelLoader ml;
 
 			ml.CreateCube(mth::float3(-0.25f), mth::float3(0.5f));
@@ -24,6 +24,8 @@ namespace TowerDefense
 
 			ml.LoadText((graphics.AppDirectory() + L"resources\\map.ttdm").c_str());
 			map.model = gfx::Model::CreateP(graphics, ml);
+			materialData = ml.Materials()[0].data;
+			material = gfx::Material::CreateP(texture, normalmap, materialData);
 			map.materials.push_back(
 				gfx::Material::CreateP(
 					gfx::Texture::CreateColoredTexture(graphics, { 55, 225, 55, 255 }, 16),
@@ -33,6 +35,8 @@ namespace TowerDefense
 
 			ml.LoadText((graphics.AppDirectory() + L"resources\\base.ttdm").c_str());
 			base.model = gfx::Model::CreateP(graphics, ml);
+			materialData = ml.Materials()[0].data;
+			material = gfx::Material::CreateP(texture, normalmap, materialData);
 			base.materials.push_back(
 				gfx::Material::CreateP(
 					gfx::Texture::CreateColoredTexture(graphics, { 225, 55, 225, 255 }, 16),
@@ -42,18 +46,24 @@ namespace TowerDefense
 
 			ml.LoadText((graphics.AppDirectory() + L"resources\\heavy.ttdm").c_str());
 			turret_heavy.model = gfx::Model::CreateP(graphics, ml);
+			materialData = ml.Materials()[0].data;
+			material = gfx::Material::CreateP(texture, normalmap, materialData);
 			for (size_t i = 0; i < ml.VertexGroups().size(); i++)
 				turret_heavy.materials.push_back(material);
 			turret_heavy.hitbox = phy::Hitbox::CreateP(ml);
 
 			ml.LoadText((graphics.AppDirectory() + L"resources\\launcher.ttdm").c_str());
 			turret_launcher.model = gfx::Model::CreateP(graphics, ml);
+			materialData = ml.Materials()[0].data;
+			material = gfx::Material::CreateP(texture, normalmap, materialData);
 			for (size_t i = 0; i < ml.VertexGroups().size(); i++)
 				turret_launcher.materials.push_back(material);
 			turret_launcher.hitbox = phy::Hitbox::CreateP(ml);
 
 			ml.LoadText((graphics.AppDirectory() + L"resources\\light.ttdm").c_str());
 			turret_light.model = gfx::Model::CreateP(graphics, ml);
+			materialData = ml.Materials()[0].data;
+			material = gfx::Material::CreateP(texture, normalmap, materialData);
 			for (size_t i = 0; i < ml.VertexGroups().size(); i++)
 				turret_light.materials.push_back(material);
 			turret_light.hitbox = phy::Hitbox::CreateP(ml);
